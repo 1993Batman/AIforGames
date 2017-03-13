fcdd8830c24d1a5e8d8e91c9bc04dae0e02ff872
1993Batman <steven.efthimiadis@gmail.com>
2017-03-14 09:05:23 +1100

    lab 5 complete

---
+++ Lab5/Lab5/hunter.py
@@ -0,0 +1,184 @@
+'''An agent with Seek, Flee, Arrive, Pursuit behaviours
+
+Created for COS30002 AI for Games by Clinton Woodward cwoodward@swin.edu.au
+
+'''
+
+from vector2d import Vector2D
+from vector2d import Point2D
+from graphics import egi, KEY
+from math import sin, cos, radians
+from random import random, randrange, uniform
+from path import Path
+from tkinter import Scale
+
+
+AGENT_MODES = {
+    KEY._1: 'seek',
+    KEY._2: 'arrive_slow',
+    KEY._3: 'arrive_normal',
+    KEY._4: 'arrive_fast',
+    KEY._5: 'flee',
+    KEY._6: 'pursuit',
+    KEY._7: 'follow_path'
+}
+
+
+class Hunter(object):
+
+    # NOTE: Class Object (not *instance*) variables!
+    DECELERATION_SPEEDS = {
+        'slow': 0.9,
+        'normal':0.5,
+        'fast': 0.2
+        ### ADD 'normal' and 'fast' speeds here
+    }
+
+    def __init__(self, world=None, scale=30.0, mass=1.0, mode='seek'):
+        # keep a reference to the world object
+        self.world = world
+        self.mode = mode
+        # where am i and where am i going? random
+        dir = radians(random()*360)
+        self.pos = Vector2D(randrange(world.cx), randrange(world.cy))
+        self.vel = Vector2D()
+        self.heading = Vector2D(sin(dir), cos(dir))
+        self.side = self.heading.perp()
+        self.scale = Vector2D(scale, scale)  # easy scaling of agent size
+        self.acceleration = Vector2D()  # current steering force
+        self.mass = mass
+        # limits?
+        self.max_speed = 1000.0
+        # data for drawing this agent
+        self.color = 'BLUE'
+        self.vehicle_shape = [
+            Point2D(-1.0,  0.6),
+            Point2D( 1.0,  0.0),
+            Point2D(-1.0, -0.6)
+        ]
+        
+        self.path = Path()
+        self.randomise_path()
+        self.waypoint_threshold = 150.0
+        self.turnRate = 0.3
+
+    def calculate(self):
+        # reset the steering force
+        mode = self.mode
+        if mode == 'seek':
+            force = self.seek(self.world.target)
+        elif mode == 'arrive_slow':
+            force = self.arrive(self.world.target, 'slow')
+        elif mode == 'arrive_normal':
+            force = self.arrive(self.world.target, 'normal')
+        elif mode == 'arrive_fast':
+            force = self.arrive(self.world.target, 'fast')
+        elif mode == 'flee':
+            force = self.flee(self.world.target, 'fast')
+        elif mode == 'pursuit':
+            force = self.pursuit(self.world.hunter)
+        else:
+            force = Vector2D()
+        self.force = force
+        return force
+
+    def update(self, delta):
+        ''' update vehicle position and orientation '''
+        force = self.calculate()
+        # new velocity
+        self.vel += force * delta
+        # check for limits of new velocity
+        self.vel.truncate(self.max_speed)
+        # update position
+        self.pos += self.vel * delta
+        # update heading is non-zero velocity (moving)
+        if self.vel.lengthSq() > 0.00000001:
+            self.heading = self.vel.get_normalised()
+            self.side = self.heading.perp()
+        # treat world as continuous space - wrap new position if needed
+        self.world.wrap_around(self.pos)
+
+    def render(self, color=None):
+        ''' Draw the triangle agent with color'''
+        egi.set_pen_color(name=self.color)
+        pts = self.world.transform_points(self.vehicle_shape, self.pos,
+                                          self.heading, self.side, self.scale)
+        # draw it!
+        egi.closed_shape(pts)
+
+    def speed(self):
+        return self.vel.length()
+
+    #--------------------------------------------------------------------------
+
+    def seek(self, target_pos):
+        ''' move towards target position '''
+        desired_vel = (target_pos - self.pos).normalise() * self.max_speed
+        return (desired_vel - self.vel)
+
+    def flee(self, hunter_pos, speed):
+        ''' move away from hunter position '''
+## add panic distance (second)
+## add flee calculations (first)
+        decel_rate = self.DECELERATION_SPEEDS[speed]
+        flee_target = self.pos - hunter_pos
+        dist = flee_target.length()
+        if dist > 15:
+            speed = dist / decel_rate
+            speed = min(speed, self.max_speed)
+            desired_vel = flee_target * (speed / dist)
+            return (desired_vel - self.vel)
+        return Vector2D()
+
+    def arrive(self, target_pos, speed):
+        ''' this behaviour is similar to seek() but it attempts to arrive at
+            the target position with a zero velocity'''
+        decel_rate = self.DECELERATION_SPEEDS[speed]
+        to_target = target_pos - self.pos
+        dist = to_target.length()
+        if dist > 0:
+            # calculate the speed required to reach the target given the
+            # desired deceleration rate
+            speed = dist / decel_rate
+            # make sure the velocity does not exceed the max
+            speed = min(speed, self.max_speed)
+            # from here proceed just like Seek except we don't need to
+            # normalize the to_target vector because we have already gone to the
+            # trouble of calculating its length for dist.
+            desired_vel = to_target * (speed / dist)
+            return (desired_vel - self.vel)
+        return Vector2D(0, 0)
+
+    def pursuit(self, evader):
+        ''' this behaviour predicts where an agent will be in time T and seeks
+            towards that point to intercept it. '''
+        evader = self.world.agents[0]
+        # assumes that evader is a Vehicle
+        toEvader = evader.pos - self.pos
+        relativeHeading = self.heading.dot(evader.heading)
+        # simple out: if target is ahead and facing us, head straight to it
+        if (toEvader.dot(self.heading)>0) and (relativeHeading < 0.95):
+            # acos(0.95)=18 degrees
+            return self.seek(evader.pos)
+        # time proportional to distance, inversely proportional to sum of velocities
+        lookAheadTime = toEvader.length() / (self.max_speed + evader.speed())
+        # turn rate delay? dot product = 1 if ahead, -1 if behind.
+        lookAheadTime += (1 - self.heading.dot(evader.pos))*- self.turnRate
+        # Seek the predicted location (using look-ahead time)
+        lookAheadPos = evader.pos + evader.vel * lookAheadTime
+        return self.seek(lookAheadPos)
+    
+    def randomise_path(self):
+        cx = self.world.cx
+        cy = self.world.cy
+        margin = min(cx,cy) * (1/6)
+        self.path.create_random_path(4, 0+margin,0+margin, self.world.cx - margin, self.world.cy - margin)
+
+    def follow_path(self):
+        if (self.path.is_finished()) :
+            return self.arrive(self.world.target, 'slow')
+        else:
+            dist = self.path.current_pt() - self.pos
+            if dist.length() < self.waypoint_threshold:
+                self.path.inc_current_pt()
+            return self.seek(self.path.current_pt())
